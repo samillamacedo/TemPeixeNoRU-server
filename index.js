26c0abc6c0a3963db14b93c70e53b417e6a02e76
Ivan Seidel <ivanseidel@gmail.com>
2016-08-09 00:14:01 -0300

    Added mock database

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
-
 const http = require('http')
+
+var dadosMock = require('./database-mock');
+
 const server = http.createServer((request, response) => {
   response.writeHead(200, {
-    'Content-Type': 'text/html',
+    'Content-Type': 'application/json; charset=utf-8',
   })
 
-  response.write('Hello World2')
+  response.write(JSON.stringify(dadosMock))
   response.end()
-
 })
 
 server.listen(process.env.PORT || 2800)
