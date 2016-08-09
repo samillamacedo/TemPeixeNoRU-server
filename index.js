41631e50350fd762e5cc9f0eabfc316f1deec100
Ivan Seidel <ivanseidel@gmail.com>
2016-08-09 00:19:31 -0300

    Added url validation

--- index.js
+++ index.js
@@ -3,14 +3,19 @@ const http = require('http')
 var dadosMock = require('./database-mock');
 
 const server = http.createServer((request, response) => {
-  console.log(request.url)
+  if(request.url != '/api/v1/cardapio.json'){
+    response.writeHead(404, {
+      'Content-Type': 'text/html; charset=utf-8',
+    })
+
+    response.end('Página não encontrada')
+  }
 
   response.writeHead(200, {
     'Content-Type': 'application/json; charset=utf-8',
   })
 
-  response.write(JSON.stringify(dadosMock))
-  response.end()
+  response.end(JSON.stringify(dadosMock))
 })
 
 server.listen(process.env.PORT || 2800)
